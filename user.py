65488e6083083d43a9c07f6890af32bc5d3edde4
Nifle CGE <pub@elfin.fr>
2022-08-06 13:42:59 +0200

    corrections d'erreur

--- user.py
+++ user.py
@@ -2,20 +2,24 @@ from azure.cosmos.exceptions import *
 import json
 
 class User:
-    def __init__(self, id_=None, name=None, email=None) -> None:
+    def __init__(self, id_="", name="", email="") -> None:
         self.id_ = id_
         self.name = name
         self.email = email
-        self.upvote = 0
-        self.downvote = 0
+        self.upvote = ""
+        self.downvote = ""
         self.last_active = 0
+
         self.banned = 0
         self.ban_message = ""
-        self.sample_reports = [] 
         self.ban_reason =  ""
+        self.most_quoted = ""
         self.ban_appeal = ""
+
         self.report_post_id = 0
         self.report_reason = ""
+        self.report_quote = ""
+
         self.is_active = True
         self.is_authenticated = False
         self.is_anonymous = False
@@ -24,7 +28,7 @@ class User:
     def get_id(self) -> str:
         return self.id_
 
-    def import_user(self, u_cont, user_id:str) -> None:
+    def uimport(self, u_cont, user_id:str) -> None:
         """
         Use this functions when sure the user is authenticated
         """
@@ -39,13 +43,18 @@ class User:
         self.upvote = user["upvote"]
         self.downvote = user["downvote"]
         self.last_active = user["last_active"]
-        self.banned = user["ban"]["status"]
-        self.ban_message = user["ban"]["message"]
-        self.sample_reports = user["ban"]["sample_reports"]
-        self.ban_reason =  user["ban"]["reason"]
-        self.ban_appeal = user["ban"]["appeal"]
-        self.report_post_id = user["report"]["post_id"]
-        self.report_reason = user["report"]["reason"]
+
+        if user.get("ban"):
+            self.banned = 1
+            self.ban_message = user["ban"]["message"]
+            self.ban_reason =  user["ban"]["reason"]
+            self.most_quoted = user["ban"]["most_quoted"]
+            self.ban_appeal = user["ban"]["appeal"]
+
+        if user.get("report"):
+            self.report_post_id = user["report"]["post_id"]
+            self.report_reason = user["report"]["reason"]
+            self.report_quote = user["report"]["quote"]
         
         self.is_active = not bool(self.banned)
         self.is_authenticated = True
@@ -54,7 +63,7 @@ class User:
 
         return True
 
-    def export_user(self, u_cont):
+    def uexport(self, u_cont):
         with open("samples/sample_user.json", "r", encoding="utf-8") as sample_user:
             user = json.load(sample_user)
 
@@ -64,14 +73,20 @@ class User:
         user["upvote"] = self.upvote
         user["downvote"] = self.downvote
         user["last_active"] = self.last_active
-        user["ban"]["status"] = self.banned
-        user["ban"]["message"] = self.ban_message
-        user["ban"]["sample_reports"] = self.sample_reports
-        user["ban"]["reason"] = self.ban_reason
-        user["ban"]["appeal"] = self.ban_appeal
-        user["report"]["post_id"] = self.report_post_id
-        user["report"]["reason"] = self.report_reason
 
-        u_cont.upsert_item(user)
+        if self.banned:
+            user["ban"] = {}
+            user["ban"]["message"] = self.ban_message
+            user["ban"]["reason"] = self.ban_reason
+            user["ban"]["most_quoted"] = self.most_quoted
+            user["ban"]["appeal"] = self.ban_appeal
+
+        if self.report:
+            user["report"] = {}
+            user["report"]["post_id"] = self.report_post_id
+            user["report"]["reason"] = self.report_reason
+            user["report"]["quote"] = self.report_quote
+
+        u_cont.replace_item(user)
 
         return True
